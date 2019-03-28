ac7ce5051ce8f9d1a09483f6ce64868ea8628cdc
ruohan.chen <crhan123@gmail.com>
2019-03-28 11:27:30 +0800

    use isort before yapf

--- pre_commit_hooks/yapf-isort.sh
+++ pre_commit_hooks/yapf-isort.sh
@@ -6,16 +6,16 @@ set -o nounset
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
 
-echo 'Begin yapf'
-if ! which yapf &>/dev/null; then
-  >&2 echo 'yapf command not found'
-  exit 1
-fi
-yapf -i -vv "$@"
-
 echo 'Begin isort'
 if ! which yapf &>/dev/null; then
   >&2 echo 'isort command not found'
   exit 1
 fi
 isort -ac "$@"
+
+echo 'Begin yapf'
+if ! which yapf &>/dev/null; then
+  >&2 echo 'yapf command not found'
+  exit 1
+fi
+yapf -i -vv "$@"
